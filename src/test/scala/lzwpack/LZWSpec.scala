7562a83e1b3583ebc2c9dfc9053e9b18e959c4bc
Sami Kukkonen <sami@samikukkonen.fi>
2018-01-28 14:49:01 +0200

    Fix decompression tests

--- src/test/scala/lzwpack/LZWSpec.scala
+++ src/test/scala/lzwpack/LZWSpec.scala
@@ -6,17 +6,20 @@ import fs2._
 class LZWSpec extends UnitSpec {
   import LZW._
 
-  describe("compress") {
-    implicit val alphabet: Alphabet[Byte] = Alphabet('A' to 'Z').map(_.toByte)
 
+  implicit val alphabet: Alphabet[Byte] = Alphabet('A' to 'Z').map(_.toByte)
+
+  describe("compress") {
     it("encodes an input string correctly") {
       val input = Stream.emits("TOBEORNOTTOBEORTOBEORNOT".getBytes)
       assertResult(List(20, 15, 2, 5, 15, 18, 14, 15, 20, 27, 29, 31, 36, 30, 32, 34, 0))(input.through(compress[Pure]).map(_._1).toList)
     }
+  }
 
+  describe("decompress") {
     it("decodes a compressed string correctly") {
       val input = Stream.emits(List(20, 15, 2, 5, 15, 18, 14, 15, 20, 27, 29, 31, 36, 30, 32, 34, 0))
-      assertResult("TOBEORNOTTOBEORTOBEORNOT")(input.through(decompress[Pure]).toList.mkString)
+      assertResult("TOBEORNOTTOBEORTOBEORNOT")(input.through(decompress[Pure]).toList.asString)
     }
   }
 }
